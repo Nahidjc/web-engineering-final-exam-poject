2fc419ac10e0ad500fdd22cc15405cf00cd71e0c
Nahidjc <nahidjc80@gmail.com>
2021-08-28 10:20:23 +0600

    array reverse

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var element = document.getElementById("submit");
 element.addEventListener('click', function () {
-    var inputValue = document.getElementById("phase").value;
+    var inputValue = document.getElementById("input-text").value;
     let filter = document.getElementById("filter").value;
+    let finalResult;
+    var wordArray = [];
     if (filter.length !== 0) {
-        var ret = inputValue.replace(filter, '');
+        var result = inputValue.replace(filter, '');
+        wordArray = result.split(' ');
+        finalResult = reverseArray(wordArray);
+
+
+    } else {
+        wordArray = inputValue.split(' ');
+
+        finalResult = reverseArray(wordArray);
+
     }
 
-    console.log(ret)
+    console.log(finalResult)
 });
+
+
+function reverseArray(arr) {
+    var newArray = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+        newArray.push(arr[i]);
+    }
+    return newArray;
+}
